0e4e8cf7f128c80518687f23e0b7197e3034c8ce
thelittlecitizen16 <thelittlecitizen16@gmail.com>
2020-08-19 09:43:50 +0300

    add timer

---
+++ OverviewII/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Globalization;
+using System.Xml;
+
+namespace OverviewII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ OverviewII/Time.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+namespace OverviewII
+{
+    public class Time
+    {
+        public  DateTime _time;
+        public Time(DateTime time)
+        {
+            _time = time;
+        }
+
+        public string PrintTheCurrentHour()
+        {
+            return _time.ToString("HH:mm:ss zzz");
+        }
+
+        public void Add(DateTime newTime)
+        {
+            _time = new DateTime(_time.Ticks + newTime.Ticks);
+        }
+
+        public void Subtraction(DateTime newTime)
+        {
+            _time = new DateTime(_time.Ticks - newTime.Ticks);
+        }
+        public List<DateTime> Sort(List<DateTime> allDates)
+        {
+           var PM = allDates
+                .Where(d => d.ToString("tt", CultureInfo.InvariantCulture) == "PM").ToList()
+                .OrderBy(d => d.TimeOfDay).ToList();
+
+            var AM = allDates
+                 .Where(d => d.ToString("tt", CultureInfo.InvariantCulture) == "AM").ToList()
+                 .OrderBy(d => d.TimeOfDay).ToList();
+
+           AM.AddRange(PM);
+
+           return AM;
+        }
+
+        public static bool operator ==(Time time1, Time time2)
+        {
+            return time1._time.TimeOfDay.Equals(time2._time.TimeOfDay);
+        }
+        public static bool operator !=(Time time1, Time time2)
+        {
+            return !(time1._time==time2._time);
+        }
+
+    }
+}
